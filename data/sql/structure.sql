0cc7882e32d55930391e44c1743d686a37b6bb03
Robert Pettet <pettet@hotmail.ca>
2020-01-24 08:28:18 -0500

    Added the mysql-table 'sessions' to the sql/structure file.

--- data/sql/structure.sql
+++ data/sql/structure.sql
@@ -11,6 +11,16 @@ create table alpha.m_ana_hits (
 	ts timestamp default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
 );
 
+create table alpha.sessions (
+	id int unsigned auto_increment primary key,
+	ip varchar(255) not null,
+    origin varchar(255),
+    user_agent text,
+	last_action longtext,
+	ts_lastseen timestamp,
+	ts_created timestamp default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
+);
+
 create table alpha.blocked_ips (
 	ip varchar(255) primary key,
 	reason longtext,
