eeb76faf59a8a8c1fbadca9ef337134aaa04913f
Robert Pettet <pettet@hotmail.ca>
2020-02-01 16:58:19 -0500

    Added more stats to mary's portal.

--- data/apps/mary/Mary.js
+++ data/apps/mary/Mary.js
@@ -11,29 +11,38 @@ function Mary(L){
 
 
 
-  var proc = child_process.spawn("dstat",["--net"]);
+  var proc = child_process.spawn("dstat",["--net","--load"]);
   let n = 0;
   proc.stdout.on("data",function(raw){
     raw = raw.toString("utf8").trim();
     n++;
-    if(n<4) return;
+    if(n<3) return;
 
-    raw = raw.split(" ");
-
-console.log(" ");
+    raw = raw.split("|");
+    raw[0] = raw[0].split(" ");
+    raw[1] = raw[1].split(" ");
 
     let stats = {
-      net_recv: raw[0],
-      net_send: raw[raw.length-1],
+      net_recv: raw[0][0],
+      net_send: raw[0][raw[0].length-1],
+      load_1m: parseFloat(raw[1][0]),
+      //load_5m: raw[1][1],
+      //load_15m: raw[1][2],
     };
 
-    console.log(stats);
+    var tickPing = performance.now();
+    process.nextTick(function(){
+      tickPing = performance.now()-tickPing;
+      //====
+      stats.tick_ms = tickPing;
+      console.log(stats);
 
-return;
-    if(stats.cpu_use>5){
-      console.log(log.COLORS.FG_RED+" CPU ALERT "+log.COLORS.RESET);
-    }
-
+      /* the highest i could acheive was 1.92 for load_1m*/
+      if(stats.load_1m>1.5){
+        console.log(log.COLORS.FG_RED+" CPU ALERT "+log.COLORS.RESET);
+      }
+      //====
+    });
   });
 
   proc.stderr.on("data",function(raw){
